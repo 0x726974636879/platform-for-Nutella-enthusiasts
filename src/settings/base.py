ff8844c264eb1d08efce6775e068ca07cf51fea7
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-04-05 21:07:03 +0200

    modify(sttings/base): STATICT_ROOT path dynamic.

--- src/settings/base.py
+++ src/settings/base.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = f'{os.path.dirname(BASE_DIR)}/static/'
 STATICFILES_DIRS = [
     f"{BASE_DIR}/static",
 ]
