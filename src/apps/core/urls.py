58af3622aa86a8e36f1c7016a49c6d3247909b88
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-04-26 23:46:55 +0200

    style(core/urls): PEP8.

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('legal_mentions/', views.LegalMentionsView.as_view(), name='legal_mentions'),
+    path(
+        'legal_mentions/',
+        views.LegalMentionsView.as_view(),
+        name='legal_mentions'
+    ),
     path('login/', views.LoginPageView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
