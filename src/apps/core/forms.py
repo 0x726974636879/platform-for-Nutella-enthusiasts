a87a793e2fa1306737c8baf8b6c89e1f4c6dfeab
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-04-26 23:48:38 +0200

    add(core/forms): ProductSearchForm.

--- src/apps/core/forms.py
+++ src/apps/core/forms.py
@@ -1,12 +1,33 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Requis.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Requis.')
-    email = forms.EmailField(max_length=254, help_text='Requis. Entrez une adresse email valide.')
+    first_name = forms.CharField(
+        max_length=30, required=False, help_text='Prenom.'
+    )
+    last_name = forms.CharField(
+        max_length=30, required=False, help_text='Nom.'
+    )
+    email = forms.EmailField(
+        max_length=254, help_text='Entrez une adresse email valide.'
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = (
+            'username', 'first_name', 'last_name', 'email', 'password1',
+            'password2'
+        )
+
+
+class ProductSearchForm(forms.Form):
+    product_name = forms.CharField(
+        help_text='Nom du produit.',
+        label="Nom du produit",
+        max_length=100,
+        min_length=2,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
