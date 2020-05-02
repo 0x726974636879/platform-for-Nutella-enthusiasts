2372f6b3529c72f0189a79bfd8001343536f88b3
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-05-03 01:49:02 +0200

    add(core/views): LoginPageVIew

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -5,58 +5,73 @@ from django.shortcuts import HttpResponseRedirect, render, redirect
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
-from .forms import ProductSearchForm, SignUpForm
+from .forms import LoginForm, ProductSearchForm, SignUpForm
 
 
 class HomePageView(FormView):
+    """
+    Home page view.
+    """
     template_name = "core/index.html"
     form_class = ProductSearchForm
 
 
-class LoginPageView(TemplateView):
-    extra_context = {
-        'focus': 'login'
-    }
-    template_name = "core/login_register.html"
+class LoginPageView(FormView):
+    """
+    Login page view.
+    """
+    form_class = LoginForm
+    template_name = "core/login.html"
 
     def post(self, request, **kwargs):
         """
         Override post method to authenticate a user.
         """
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            user = None
-        is_correct_password = user.check_password(password)
-        if user is not None and is_correct_password and user.is_active:
-            user = authenticate(username=user.username, password=password)
-            login(request, user)
-            return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            # Retrieve email and password informations.
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            # Get the user.
+            try:
+                user = User.objects.get(email=email)
+                print(1)
+            except User.DoesNotExist:
+                error_msg = "L'adresse email renseignée n'est pas valide."
+                form.add_error("email", error_msg)
+                return render(request, self.template_name, {"form": form})
+            # Try to connect with the user.
+            is_correct_password = user.check_password(password)
+            if user is not None and is_correct_password and user.is_active:
+                user = authenticate(username=user.username, password=password)
+                login(request, user)
+                return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+            elif not is_correct_password:
+                form.add_error("password", "Mot de passe incorrect.")
+            return render(request, self.template_name, {"form": form})
 
-        return render(request, self.template_name)
+        return render(request, self.template_name, {"form": form})
 
 
 class LogoutView(TemplateView):
-    extra_context = {
-        'focus': 'login'
-    }
-    template_name = "core/login_register.html"
+    """
+    Logout page view.
+    """
 
     def get(self, request, **kwargs):
         """
         Override get method to logout the user.
         """
         logout(request)
-        return redirect('home')
+        return redirect("home")
 
 
-class SignUpView(TemplateView):
-    extra_context = {
-        'focus': 'signup'
-    }
-    template_name = "core/login_register.html"
+class SignUpView(FormView):
+    """
+    Sign up page view.
+    """
+    form_class = SignUpForm
+    template_name = "core/signup.html"
 
     def post(self, request, **kwargs):
         """
@@ -66,13 +81,16 @@ class SignUpView(TemplateView):
         if form.is_valid():
             user = form.save()
             user = authenticate(
-                username=form.cleaned_data.get('username'),
-                password=form.cleaned_data.get('password1')
+                username=form.cleaned_data.get("username"),
+                password=form.cleaned_data.get("password1")
             )
             login(request, user)
-            return redirect('home')
-        return render(request, self.template_name, {'form': form})
+            return redirect("home")
+        return render(request, self.template_name, {"form": form})
 
 
 class LegalMentionsView(TemplateView):
+    """
+    Legal mentions page view.
+    """
     template_name = "core/legal_mentions.html"
