5a4e88b2efe377a583ace0675c3fc96fb22bbaa6
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-05-03 01:46:32 +0200

    add(core/forms): LoginForm.

--- src/apps/core/forms.py
+++ src/apps/core/forms.py
@@ -3,15 +3,72 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        help_text='Entrez une adresse email valide.',
+        label="Adresse email",
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    password = forms.CharField(
+        help_text="Entrez votre mot de passe.",
+        label="Mot de passe",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        help_text='Entrez votre pseudo.',
+        label="Pseudo",
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
     first_name = forms.CharField(
-        max_length=30, required=False, help_text='Prenom.'
+        help_text='Entrez votre prenom.',
+        label="Prenom",
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
     )
     last_name = forms.CharField(
-        max_length=30, required=False, help_text='Nom.'
+        help_text='Entrez votre nom.',
+        label="Nom",
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
     )
     email = forms.EmailField(
-        max_length=254, help_text='Entrez une adresse email valide.'
+        help_text='Entrez une adresse email valide.',
+        label="Adresse email",
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    password1 = forms.CharField(
+        help_text="Entrez votre mot de passe.",
+        label="Mot de passe",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    password2 = forms.CharField(
+        help_text="Entrez le même mot de passe que ci-dessus, pour vérification.",
+        label="Verification de mot de passe.",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
     )
 
     class Meta:
