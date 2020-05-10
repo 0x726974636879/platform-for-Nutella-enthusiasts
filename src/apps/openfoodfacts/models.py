0bfbfbc48e2394f46d87078343bdf4084bad082f
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-05-10 20:07:34 +0200

    add(openfoodfacts): Product model.

--- src/apps/openfoodfacts/models.py
+++ src/apps/openfoodfacts/models.py
@@ -19,9 +19,10 @@ class Product(models.Model):
     """
     Product model.
     """
-    product_name = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=400)
     code = models.CharField(max_length=30)
     img_url = models.CharField(max_length=400, blank=True, null=True)
+    url = models.CharField(max_length=400, blank=True, null=True)
     salt = models.CharField(max_length=10, blank=True, null=True)
     fat = models.CharField(max_length=10, blank=True, null=True)
     sugars = models.CharField(max_length=10, blank=True, null=True)
@@ -33,6 +34,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+    @classmethod
+    def get_substitutes(cls, category_id):
+        """
+        Get a list of better products than the one chosen.
+
+        Parameters
+        ----------
+        category_id : int
+            Product category identifier.
+
+        Returns
+        -------
+        products: tuple
+            List of products that can replace the selected product.
+        """
+        # Get products from a category sorted by nutrition grades.
+        col_name = "nutrition_grades"
+        products = tuple(
+            cls.objects.filter(category=category_id).order_by(col_name)[:12]
+        )
+        return products
 
 
 class BackupProduct(models.Model):
