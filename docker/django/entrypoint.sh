365c8f1a6ac64d1c86ebe385c96d5996c174cc47
mnrdev <mnrdev@mnrs-MacBook-Pro.local>
2020-09-28 00:08:20 +0200

    modify(docker/django/entrypoint): launch the project with settings.base instead settaings.local

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -2,6 +2,6 @@
 
 pip install -r /usr/src/app/requirements.txt
 
-python /usr/src/app/src/manage.py runserver 0:8000 --settings=settings.local
+python /usr/src/app/src/manage.py runserver 0:8000 --settings=settings.base
 
 exec "$@"
